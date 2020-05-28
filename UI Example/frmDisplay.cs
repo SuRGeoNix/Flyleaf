1a179db69b96075dd29ff2f0e4e89ea8364412c4
SuRGeoNix <SuRGeoNix@gmail.com>
2020-05-28 20:55:19 +0300

    UI Updates (Mainly fixing Monogame's loop for better performance) Torrent Streaming Updates (Faster streaming by updating TorSwarm lib & by disabling embded subs by default) Subtitles Issues FFmpeg Libraries Update to latest (except swscale) Added 'H" key for HW Accel On/Off (requires re-opening input) Code Clean-Up

--- UI Example/frmDisplay.cs
+++ UI Example/frmDisplay.cs
@@ -9,17 +9,9 @@ namespace PartyTime.UI_Example
         SpriteBatch             spriteBatch;
         UserInterface           UI;
 
-        public frmDisplay()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
-        protected override void Initialize()                { base.Initialize(); /*this.TargetElapsedTime = TimeSpan.FromSeconds(1.0f / 60.0f);*/ }
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            UI = new UserInterface(this, ref graphics, ref spriteBatch);
-        }
+        public                  frmDisplay()                { graphics = new GraphicsDeviceManager(this); Content.RootDirectory = "Content"; }
+        protected override void Initialize()                { base.Initialize(); this.TargetElapsedTime = System.TimeSpan.FromSeconds(1.0f / 2.0f); /*this.IsFixedTimeStep = true; this.TargetElapsedTime = System.TimeSpan.FromSeconds(1.0f / 5.0f);*/ }
+        protected override void LoadContent()               { spriteBatch = new SpriteBatch(GraphicsDevice); UI = new UserInterface(this, ref graphics, ref spriteBatch); }
         protected override void UnloadContent()             { }
         protected override void Update(GameTime gameTime)   { UI.UpdateLoop(); SuppressDraw(); base.Update(gameTime); }
         protected override bool BeginDraw()                 { return false; }
