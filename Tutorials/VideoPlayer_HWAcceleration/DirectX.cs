242b487e7fd41d4a3b9c089ffc73ab5fd9773960
SuRGeoNix <SuRGeoNix@gmail.com>
2020-06-04 16:26:03 +0300

    Adding HWAcceleration Tutorial
    C# Video Demuxing & Hardware(GPU) Decoding / Processing Acceleration Tutorial

--- Tutorials/VideoPlayer_HWAcceleration/DirectX.cs
+++ Tutorials/VideoPlayer_HWAcceleration/DirectX.cs
@@ -41,14 +41,14 @@ namespace VideoPlayer_HWAcceleration
         public DirectX(IntPtr outputHandle) { Initialize(outputHandle); }
         #endregion
 
-        // 
+        // Get's a handle to assosiate with the BackBuffer and Prepares Devices
         private void Initialize(IntPtr outputHandle)
         {
             // SwapChain Description
             var desc = new SwapChainDescription()
             {
                 BufferCount         = 1,
-                ModeDescription     = new ModeDescription(0, 0, new Rational(0, 0), Format.B8G8R8A8_UNorm),
+                ModeDescription     = new ModeDescription(0, 0, new Rational(0, 0), Format.B8G8R8A8_UNorm), // RBGA | BGRA 32-bit
                 IsWindowed          = true,
                 OutputHandle        = outputHandle,
                 SampleDescription   = new SampleDescription(1, 0),
@@ -60,14 +60,15 @@ namespace VideoPlayer_HWAcceleration
             Device.CreateWithSwapChain(SharpDX.Direct3D.DriverType.Hardware, DeviceCreationFlags.Debug | DeviceCreationFlags.BgraSupport, desc, out _device, out _swapChain);
             _backBuffer     = Texture2D.FromSwapChain<Texture2D>(_swapChain, 0);
 
-            // Ignore all windows events
+            // Creates Association between outputHandle and BackBuffer
             var factory = _swapChain.GetParent<Factory>();
             factory.MakeWindowAssociation(outputHandle, WindowAssociationFlags.IgnoreAll);
 
-            // Prepare Video Processor Emulator | Input | Output | Stream
+            // Video Device | Video Context
             videoDevice1    = _device.QueryInterface<VideoDevice1>();
             videoContext1   = _device.ImmediateContext.QueryInterface<VideoContext1>();
 
+            // Creates Video Processor Enumerator
             vpcd = new VideoProcessorContentDescription()
             {
                 Usage = VideoUsage.PlaybackNormal,
@@ -82,10 +83,10 @@ namespace VideoPlayer_HWAcceleration
                 InputHeight = 1,
                 OutputHeight = 1
             };
-
             videoDevice1.CreateVideoProcessorEnumerator(ref vpcd, out vpe);
             videoDevice1.CreateVideoProcessor(vpe, 0, out videoProcessor);
             
+            // Prepares Video Processor Input View Description for Video Processor Input View that we pass Shared NV12 Texture (nv12SharedResource) each time
             vpivd = new VideoProcessorInputViewDescription()
             {
                 FourCC = 0,
@@ -93,13 +94,28 @@ namespace VideoPlayer_HWAcceleration
                 Texture2D = new Texture2DVpiv() { MipSlice = 0, ArraySlice = 0 }
             };
 
+            // Creates Video Processor Output to our BackBuffer
             vpovd = new VideoProcessorOutputViewDescription() { Dimension = VpovDimension.Texture2D };
-
             videoDevice1.CreateVideoProcessorOutputView((Resource) _backBuffer, vpe, vpovd, out vpov);
 
+            // Prepares Streams Array
             vpsa = new VideoProcessorStream[1];
         }
 
+        /* ID3D11VideoContext::VideoProcessorBlt | https://docs.microsoft.com/en-us/windows/win32/api/d3d11/nf-d3d11-id3d11videocontext-videoprocessorblt
+         * 
+         * HRESULT VideoProcessorBlt (
+         * ID3D11VideoProcessor               *pVideoProcessor,
+         * ID3D11VideoProcessorOutputView     *pView,
+         * UINT                               OutputFrame,
+         * UINT                               StreamCount,
+         * const D3D11_VIDEO_PROCESSOR_STREAM *pStreams );
+         * 
+         * 1. Opens Shared NV12 Texture (nv12SharedResource) on our SharpDX ID3Device from FFmpeg's ID3Device
+         * 2. Creates a new Video Processor Input View that we pass in Video Processor Streams
+         * 3. Calls Video Processor Blt to convert (in GPU) Shared NV12 Texture to our BackBuffer RBGA/BGRA Texture
+         * 4. Finally Presents the Frame to the outputHandle (SampleUI Form)
+         */
         public void PresentFrame(IntPtr nv12SharedResource)
         {
             Texture2D nv12SharedTexture = _device.OpenSharedResource<Texture2D>(nv12SharedResource);
--- Tutorials/VideoPlayer_HWAcceleration/SampleUI.cs
+++ Tutorials/VideoPlayer_HWAcceleration/SampleUI.cs
@@ -16,29 +16,34 @@ namespace VideoPlayer_HWAcceleration
     {
         string      fileToPlay = @"C:\FILENAME_HERE.mp4";
 
-        FFmpeg      ffmpeg;
-        DirectX     directX;
-        Thread      threadPlay;
+        FFmpeg      ffmpeg;         // FFmpeg  Video Decoder    (GPU Acceleration)
+        DirectX     directX;        // DirectX Video Processing (GPU Acceleration)
+        Thread      threadPlay;     // Fake Play Thread with Sleep
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ffmpeg  = new FFmpeg(1);
-            directX = new DirectX(this.Handle);
+            directX = new DirectX(this.Handle); // Parses SampleUI 's Handle for output
 
             if ( !ffmpeg.Open(fileToPlay) ) { MessageBox.Show("Decoder failed to open input"); return; }
+            if ( !ffmpeg.hwAccelStatus )    { MessageBox.Show("FFmpeg failed to initialize D3D11VA device"); return; }
 
-            if ( !ffmpeg.hwAccelStatus ) { MessageBox.Show("FFmpeg failed to initialize D3D11VA device"); return; }
-
+            /* FFmpeg  Decode  Frame
+             * DirectX Process & Present Frame
+             */
             threadPlay = new Thread(() =>
             {
                 while ( true )
                 {
+                    // FFmpeg  Decode  Frame
                     IntPtr curFrame = ffmpeg.GetFrame();
                     if ( curFrame == IntPtr.Zero && !ffmpeg.hwAccelStatus) { MessageBox.Show("Pixel Format not supported by GPU"); return; }
                     if ( curFrame == IntPtr.Zero ) continue;
 
+                    //DirectX Process & Present Frame
                     directX.PresentFrame(curFrame);
 
+                    // Fake Play Thread with Sleep
                     Thread.Sleep(30);
                 }
             });
@@ -47,6 +52,7 @@ namespace VideoPlayer_HWAcceleration
             threadPlay.Start();
         }
 
+        // Misc
         public SampleUI() { InitializeComponent(); }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e) { if ( threadPlay != null && threadPlay.IsAlive ) threadPlay.Abort(); }
     }
