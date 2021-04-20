bb9e2d97510e820e339baafa2f14cb57dc1b5b82
SuRGeoNix <surgeonix@gmail.com>
2021-04-20 12:47:59 +0300

    Fixing WPF ViewModel for Customization & Updating Sample2 as reference for a ViewModel Base

--- FlyleafLib.Controls.WPF/VideoView.cs
+++ FlyleafLib.Controls.WPF/VideoView.cs
@@ -23,8 +23,15 @@ namespace FlyleafLib.Controls.WPF
 
         public Player           Player      { get; private set; }
         public FlyleafWindow    WindowFront { get; set; }
-        public Window           WindowBack  { get; set; }
-        public VideoView        FlyleafView { get; set; }
+        public Window           WindowBack  => WindowFront.WindowBack;
+
+        public VideoView FlyleafView
+        {
+            get { return (VideoView)GetValue(FlyleafViewProperty); }
+            set { SetValue(FlyleafViewProperty, value); }
+        }
+        public static readonly DependencyProperty FlyleafViewProperty =
+            DependencyProperty.Register("FlyleafView", typeof(VideoView), typeof(VideoView), new PropertyMetadata(null));
 
         public VideoView() { DefaultStyleKey = typeof(VideoView); }
         public override void OnApplyTemplate()
@@ -46,9 +53,10 @@ namespace FlyleafLib.Controls.WPF
             // TBR: Parsing the Player and VideoView to Control/ViewModel
             if (curContent != null && curContent is IVideoView) ((IVideoView)curContent).FlyleafView = this;
             WindowFront.SetContent((UIElement) curContent);
+            WindowFront.DataContext = DataContext;
+            FlyleafView = this;
             Player.Start();
         }
-
         protected override void OnContentChanged(object oldContent, object newContent)
         {
             if (IsUpdatingContent | IsDesignMode) return;
@@ -61,6 +69,60 @@ namespace FlyleafLib.Controls.WPF
 
                 WindowFront.SetContent((UIElement)newContent);
             }
+        }
+
+
+        public bool IsFullScreen { get ; private set; }
+
+        object  oldContent, oldParent;
+        public bool FullScreen()
+        {
+            if (WindowBack == null) return false;
+
+            oldParent = Parent;
+
+            if (Parent is StackPanel)
+                ((StackPanel)Parent).Children.Remove(this);
+            else if (Parent is Grid)
+                ((Grid)Parent).Children.Remove(this);
+            else
+                return false;
+
+            oldContent  = WindowBack.Content;
+            WindowBack.Content = this;
+
+            WindowBack.ResizeMode   = ResizeMode.NoResize;
+            WindowBack.WindowStyle  = WindowStyle.None;
+            WindowBack.WindowState  = WindowState.Maximized;
+
+            IsFullScreen = true;
+            WindowFront.Activate();
+
+            return true;
+        }
+
+        public bool NormalScreen()
+        {
+            if (oldParent == null || WindowBack == null) return false;
+
+            WindowBack.Content = null;
+            WindowBack.Content = oldContent;
+
+            if (oldParent is StackPanel)
+                ((StackPanel)oldParent).Children.Add(this);
+            else if (oldParent is Grid)
+                ((Grid)oldParent).Children.Add(this);
+            else
+                return false;
+
+            WindowBack.ResizeMode   = ResizeMode.CanResize;
+            WindowBack.WindowStyle  = WindowStyle.SingleBorderWindow;
+            WindowBack.WindowState  = WindowState.Normal;
+
+            IsFullScreen = false;
+            WindowFront.Activate();
+
+            return true;
         }
     }
 }
