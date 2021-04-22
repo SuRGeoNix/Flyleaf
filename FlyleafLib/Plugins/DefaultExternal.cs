56414af5f918dea603a3f46f08c22032a18a6ed3
SuRGeoNix <surgeonix@gmail.com>
2021-04-22 18:06:54 +0300

    - Adding external support to open any custom System.IO.Stream - Finishing up Default plugins (Renaming ExternalSubs to DefaultExternal which handles also the custom video stream) - Fixing BitSwarm plugin to support completed files - Fixing AssemblyResolver to not mess generally with the main application - Fxing Public setter for PreventAborts - Fixing gitignore to not ignore the actual code for Plugins just the binaries
    Former-commit-id: be054cdd77e27459a819a2dc21ffcc8670459048

---
+++ FlyleafLib/Plugins/DefaultExternal.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+using FlyleafLib.Plugins.MediaStream;
+
+namespace FlyleafLib.Plugins
+{
+    public class DefaultExternal : PluginBase, IPluginExternal
+    {
+        public bool IsPlaylist => false;
+
+        public bool Download(SubtitleStream stream) { return true; }
+
+        public SubtitleStream OpenSubtitles(string url)
+        {
+            foreach(var stream in SubtitleStreams)
+                if (stream.DecoderInput.Url == url || stream.Tag.ToString() == url) return stream;
+
+            SubtitleStreams.Add(new SubtitleStream()
+            {
+                DecoderInput= new DecoderInput() { Url = url },
+                Downloaded  = true,
+                Tag         = url // Use it here because of possible convert to Utf8 and rename
+            });
+
+            return SubtitleStreams[SubtitleStreams.Count - 1];
+        }
+        public SubtitleStream OpenSubtitles(SubtitleStream stream)
+        {
+            foreach(var sstream in SubtitleStreams)
+                if (sstream.Tag == stream.Tag) return stream;
+
+            return null;
+        }
+        public SubtitleStream OpenSubtitles(Language lang) { return null; }
+
+        public VideoStream OpenVideo(Stream stream)
+        {
+            VideoStreams.Add(new VideoStream() { DecoderInput = new DecoderInput() { Stream = stream } });
+            return VideoStreams[VideoStreams.Count - 1];
+        }
+        public OpenVideoResults OpenVideo() { return null; }
+        public VideoStream OpenVideo(VideoStream stream) { return stream; }
+
+        public void Search(Language lang) { }
+    }
+}
