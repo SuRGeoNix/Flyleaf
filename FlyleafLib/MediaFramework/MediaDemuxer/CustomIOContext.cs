0cc3ebe9034f72cbe984f9b717cabec8909723e7
SuRGeoNix <surgeonix@gmail.com>
2021-12-24 20:40:34 +0200

    CustomIOContext: Gets rid of the extra Marshal.Copy of byte arrays by using Span bytes (not supported on .NET framework)
    Updates BitSwarm to 2.4.4 which supports Span bytes

--- FlyleafLib/MediaFramework/MediaDemuxer/CustomIOContext.cs
+++ FlyleafLib/MediaFramework/MediaDemuxer/CustomIOContext.cs
@@ -13,7 +13,9 @@ namespace FlyleafLib.MediaFramework.MediaDemuxer
         AVIOContext*    avioCtx;
         public Stream   stream;
         const int       bufferSize = 0x200000; // Should be exposed to config as well
+#if NETFRAMEWORK
         byte[]          buffer;
+#endif
         Demuxer         demuxer;
 
         public CustomIOContext(Demuxer demuxer)
@@ -28,11 +30,12 @@ namespace FlyleafLib.MediaFramework.MediaDemuxer
         {
             this.stream = stream;
             //this.stream.Seek(0, SeekOrigin.Begin);
-
+#if NETFRAMEWORK
             if (buffer == null)
                 buffer  = new byte[bufferSize]; // NOTE: if we use small buffer ffmpeg might request more than we suggest
+#endif
 
-            avioCtx = avio_alloc_context((byte*)av_malloc(bufferSize), bufferSize, 0, (void*) GCHandle.ToIntPtr(demuxer.handle), ioread, null, ioseek);
+            avioCtx = avio_alloc_context((byte*)av_malloc(bufferSize), bufferSize, 0, (void*) GCHandle.ToIntPtr(demuxer.handle), ioread, null, ioseek);            
             demuxer.FormatContext->pb     = avioCtx;
             demuxer.FormatContext->flags |= AVFMT_FLAG_CUSTOM_IO;
         }
@@ -53,15 +56,19 @@ namespace FlyleafLib.MediaFramework.MediaDemuxer
 
         avio_alloc_context_read_packet IORead = (opaque, buffer, bufferSize) =>
         {
+            int ret;
             GCHandle demuxerHandle = (GCHandle)((IntPtr)opaque);
             Demuxer demuxer = (Demuxer)demuxerHandle.Target;
 
             if (demuxer.Interrupter.ShouldInterrupt(demuxer) != 0) return AVERROR_EXIT;
-            int ret = demuxer.CustomIOContext.stream.Read(demuxer.CustomIOContext.buffer, 0, bufferSize);
+#if NETFRAMEWORK
+            ret = demuxer.CustomIOContext.stream.Read(demuxer.CustomIOContext.buffer, 0, bufferSize);
             if (ret < 0) { demuxer.Log("CustomIOContext Interrupted"); return AVERROR_EXIT; }
-
             Marshal.Copy(demuxer.CustomIOContext.buffer, 0, (IntPtr) buffer, ret);
-
+#else
+            ret = demuxer.CustomIOContext.stream.Read(new Span<byte>(buffer, bufferSize));
+#endif
+            if (ret < 0) { demuxer.Log("CustomIOContext Interrupted"); return AVERROR_EXIT; }
             return ret;
         };
 
