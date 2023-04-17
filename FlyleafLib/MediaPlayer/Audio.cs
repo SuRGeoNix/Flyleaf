755cedfb4efc184ef9b5fbdfdd2a28cde4a62331
SuRGeoNix <surgeonix@gmail.com>
2023-04-18 01:08:48 +0300

    FlyleafHost.Wpf: Fixes windows relationship to parent/child to make it feel more like an embedded control (related to #255, #257, #265, #277)
    - Properly sets Parent/Owner and leaves the Owner active
    - Removes bindings for Width/Height/etc. and only refreshes overlay from surface location/size
    - Removes all styles/exStyles (including WS_SYSMENU)
    - Adds DetachedShowInTaskbar, DetachedShowInTaskbarNoOwner for more customization whether should be visible in taskbar and minimized with the owner

--- FlyleafLib/MediaPlayer/Audio.cs
+++ FlyleafLib/MediaPlayer/Audio.cs
@@ -284,7 +284,7 @@ public class Audio : NotifyPropertyChanged
             if (CanDebug) player.Log.Debug($"[Audio] Add samples failed ({e.Message})");
         }
     }
-    internal long GetDeviceDelay() => (long) (xaudio2.PerformanceData.CurrentLatencyInSamples * deviceDelayTimebase + 80000); // TODO: VBlack delay (8ms correction for now)
+    internal long GetDeviceDelay() => (long) ((xaudio2.PerformanceData.CurrentLatencyInSamples * deviceDelayTimebase) - 80000); // TODO: VBlack delay (8ms correction for now)
     internal void ClearBuffer()
     {
         lock (locker)
