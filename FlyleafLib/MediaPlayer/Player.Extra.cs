6ca01538906bb9ac574675167a08c63be9d27c43
BenW <BenW@ARCH-52>
2024-07-29 16:18:45 -0700

    ShowFrameNext now uses similar logic to ShowFramePrev

--- FlyleafLib/MediaPlayer/Player.Extra.cs
+++ FlyleafLib/MediaPlayer/Player.Extra.cs
@@ -163,7 +163,23 @@ unsafe partial class Player
                 renderer.ClearOverlayTexture();
 
             if (VideoDecoder.Frames.IsEmpty)
-                vFrame = VideoDecoder.GetFrameNext();
+            {
+                int askedFrame = VideoDecoder.GetFrameNumber(CurTime) + 1;
+                //Log.Debug($"CurTime1: {TicksToTime(CurTime)}, Asked: {askedFrame}");
+                vFrame = VideoDecoder.GetFrame(askedFrame);
+                if (vFrame == null)
+                    return;
+                int recvFrame = VideoDecoder.GetFrameNumber(vFrame.timestamp);
+                //Log.Debug($"CurTime2: {TicksToTime(vFrame.timestamp)}, Got: {recvFrame}");
+                if (askedFrame != recvFrame)
+                {
+                    VideoDecoder.DisposeFrame(vFrame);
+                    vFrame = null;
+                    vFrame = askedFrame < recvFrame
+                        ? VideoDecoder.GetFrame(VideoDecoder.GetFrameNumber(CurTime))
+                        : VideoDecoder.GetFrame(VideoDecoder.GetFrameNumber(CurTime) + 2);
+                }
+            }
             else
                 VideoDecoder.Frames.TryDequeue(out vFrame);
 
