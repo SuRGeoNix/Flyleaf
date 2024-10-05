32dba15ba8aa962bac18c9696ac47c9e34df175a
SuRGeoNix <surgeonix@gmail.com>
2024-10-05 10:54:27 +0300

    Tests/WpfFlyleafHost: Adds a sample with custom template of FlyleafME control which includes a sidebar overlayed [Related #509]

---
+++ Tests/WpfFlyleafHost/FlyleafMEwithSideBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FlyleafLib.MediaPlayer;
+using System.Windows;
+
+namespace WpfFlyleafHost;
+
+public partial class FlyleafMEwithSideBar : Window
+{
+    public Player Player { get; set; } = new Player();
+
+    public FlyleafMEwithSideBar()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+}
