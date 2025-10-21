262839d2230e0c21656be0b133384835d4fd10c1
SuRGeoNix <surgeonix@gmail.com>
2025-10-21 15:24:50 +0300

    Updates to v3.9.4 (FlyleafLib) / v1.5.4 (FlyleafME) / v1.2.4 (FlyleafHost.WinUI)
    - Engine: Prevents enumeration of A/V capture devices during start to avoid delays
    - VideoDecoder/VideoStream: Fixes an issue while FFmpeg announces 60.00x fps instead of 60fps that could cause video decoder to drop frames (based on Config.Video.MaxOutputFps limit)
    - Player.ScreamerVASD: Adds an ~60 FPS idle renderer embedded during playback to provide smoother resize and filter updates when the source FPS or speed is low
    - Player.Audio: Few stability improvements (mainly for non-analyzed inputs)
    
    [Breaking Changes]
    - Engine: A/V captures devices will require manual enumeration with Engine.Audio/Video.RefreshCapDevices() if needed
    - Config.Decoder: Defaults MaxAudioFrames from 10 to 5 for better syncing, smooth speed changes and resolves possible issues with MaxLatency

--- FlyleafLib/MediaFramework/MediaContext/DecoderContext.cs
+++ FlyleafLib/MediaFramework/MediaContext/DecoderContext.cs
@@ -631,6 +631,7 @@ public unsafe partial class DecoderContext : PluginHandler
                         }
 
                         // Accurate seek with +- half frame distance
+                        // TBR: Live streams should never been seeked at first place (maybe allow HLSLive?) * can cause infinite loop
                         if (timestamp != -1 && !VideoDemuxer.IsLive && (long)(frame->pts * VideoStream.Timebase) - VideoDemuxer.StartTime + (VideoStream.FrameDuration / 2) < timestamp)
                         {
                             av_frame_unref(frame);
