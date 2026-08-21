SuRGeoNix/Flyleaf

commit 0a214931e82d8e430211037e949683b7194f37f6

VideoDecoder: Fixes a possible infinite loop during draining
(+Misc)

FlyleafLib/MediaFramework/MediaDecoder/VideoDecoder.cs
@@ -383,8 +383,14 @@ public unsafe class VideoDecoder : DecoderBase
             }
 
             // While Packets Queue Empty (Drain | Quit if Demuxer stopped | Wait until we get packets)
-            if (vPackets.IsEmpty && !isDraining)
+            if (vPackets.IsEmpty)
             {
+                if (isDraining)
+                {
+                    Status = Status.Ended;
+                    break;
+                }
+
                 CriticalArea = true;
 
                 lock (lockStatus)
@@ -492,6 +498,12 @@ public unsafe class VideoDecoder : DecoderBase
                 ret = SendAVPacket(packet);
                 if (ret != 0)
                 {
+                    if (isDraining)
+                    {
+                        Status = Status.Ended;
+                        break;
+                    }
+
                     if (ret == AVERROR_EAGAIN)
                     {   // Fast retry => Legitimate decoding errors | Waiting for key packet
                         while (Status == Status.Running && ret == AVERROR_EAGAIN && (packet = vPackets.Dequeue()) != null)
@@ -536,7 +548,6 @@ public unsafe class VideoDecoder : DecoderBase
                 if (CanDebug) Log.Debug("Ignoring non-key packet");
                 av_packet_free(&packet);
                 return AVERROR_EAGAIN;
-                
             }
 
             keyFrameRequired  = checkKeyFrame && packet->pts != startPts;

FlyleafLib/Engine/Engine.Audio.cs
@@ -83,7 +83,7 @@ public class AudioEngine : CallbackBase, IMMNotificationClient, INotifyPropertyC
 
             var defaultDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             if (defaultDevice == null)
-            {
+            {   // TBR: New devices can be connected later on
                 Failed = true;
                 return;
             }

FlyleafLib/Engine/Globals.cs
@@ -31,18 +31,18 @@ public enum MediaType
 }
 public enum InputType
 {
-    File    = 0,
-    UNC     = 1,
-    Torrent = 2,
-    Web     = 3,
-    Unknown = 4
+    File,
+    UNC,
+    Torrent,
+    Web,
+    Unknown
 }
-public enum HDRtoSDRMethod : int
+public enum HDRtoSDRMethod // Must match pixel shader
 {
-    None        = 0,
-    Aces        = 1,
-    Hable       = 2,
-    Reinhard    = 3
+    None,
+    Aces,
+    Hable,
+    Reinhard
 }
 
 public enum DeInterlace // Must match with VideoFrameFormat
@@ -75,18 +75,18 @@ public enum Cropping
     Codec   = 1 << 1,
     Texture = 1 << 2
 }
-public enum ColorSpace : int
+public enum ColorSpace
 {
-    None        = 0,
-    Bt601       = 1,
-    Bt709       = 2,
-    Bt2020      = 3
+    None,
+    Bt601,
+    Bt709,
+    Bt2020
 }
-public enum ColorRange : int
+public enum ColorRange
 {
-    None        = 0,
-    Full        = 1,
-    Limited     = 2
+    None,
+    Full,
+    Limited
 }
 public enum ColorType
 {
@@ -94,14 +94,13 @@ public enum ColorType
     RGB,
     Gray
 }
-public enum HDRFormat : int
+public enum HDRFormat
 {
-    None        = 0,
-    DolbyVision = 1,
-    HDR         = 2,
-    HDRPlus     = 3,
-    HLG         = 4,
-    
+    None,
+    DolbyVision,
+    HDR,
+    HDRPlus,
+    HLG
 }
 public enum UIRefreshType
 {
@@ -134,8 +133,8 @@ public class GPUOutput
     public int              Top             { get; internal set; }
     public int              Right           { get; internal set; }
     public int              Bottom          { get; internal set; }
-    public int              Width           => Right- Left;
-    public int              Height          => Bottom- Top;
+    public int              Width           => Right - Left;
+    public int              Height          => Bottom - Top;
     public bool             IsAttached      { get; internal set; }
     public ModeRotation     Rotation        { get; internal set; }
     public float            MaxLuminance    { get; internal set; }
@@ -175,7 +174,7 @@ public enum GPUVendor : uint
     Nvidia      = 0x10DE,
     Qualcomm    = 0x4D4F4351,
     S3Graphics  = 0x5333,
-    VIA         = 0x1106,
+    VIA         = 0x1106
 }
 
 public struct AspectRatio : IEquatable<AspectRatio>
@@ -194,7 +193,7 @@ public struct AspectRatio : IEquatable<AspectRatio>
         new(4,      3),
         new(16,     9),
         new(16,     10),
-        new(2.35f,  1),
+        new(2.35f,  1)
     ];
 
     public static implicit operator AspectRatio(string value) => new(value);

FlyleafLib/MediaFramework/MediaDemuxer/Demuxer.cs
@@ -1075,13 +1075,16 @@ public unsafe class Demuxer : RunThreadBase
                         Log.Warn($"Seek failed 2/2 {FFmpegEngine.ErrorCodeToMsg(ret)} ({ret})");
 
                         if (fmtCtx->pb != null)
-                        {   // Fix pb after possible interrupt (reset pb to last pos otherwise will be eof) - Mainly for NoTimestamps (TODO: byte seek/calc dur/percentage)
+                        {   // Fix pb after possible interrupt
+                            if (fmtCtx->pb->error == AVERROR_EXIT)
+                                fmtCtx->pb->error = 0;
+
+                            // TBR: Reset position (from eof?) (Mainly for NoTimestamps)
                             avio_flush(fmtCtx->pb);
-                            fmtCtx->pb->error = 0;
-                            fmtCtx->pb->eof_reached = 0;
-                            avio_seek(fmtCtx->pb, savedPbPos, 0);
+                            if (avio_seek(fmtCtx->pb, savedPbPos, IOSeekFlags.Begin) > 0)
+                                fmtCtx->io_repositioned = 1;
+                            _ = avformat_flush(fmtCtx);
                         }
-                        _ = avformat_flush(fmtCtx);
                     }
                     else
                         lastSeekTime = ticks - StartTime - (hlsCtx != null ? hlsStartTime : 0);

commit 9c43c9eb6d9c2edfdc3a7d4d6c6711b78de486a2

VideoDecoder: Partially reverts 0a21493 which introduced draining issue

FlyleafLib/MediaFramework/MediaDecoder/VideoDecoder.cs
@@ -383,14 +383,8 @@ public unsafe class VideoDecoder : DecoderBase
             }
 
             // While Packets Queue Empty (Drain | Quit if Demuxer stopped | Wait until we get packets)
-            if (vPackets.IsEmpty)
+            if (vPackets.IsEmpty && !isDraining)
             {
-                if (isDraining)
-                {
-                    Status = Status.Ended;
-                    break;
-                }
-
                 CriticalArea = true;
 
                 lock (lockStatus)